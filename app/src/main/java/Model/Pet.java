c74c3ed17901011f550ae670ce5758585efdbc10
Daron <daronbaltazar@gmail.com>
2025-09-16 11:35:18 -0500

    pet class and arrays xml
    pet class and arrays xml

--- app/src/main/java/Model/Pet.java
+++ app/src/main/java/Model/Pet.java
@@ -1,4 +1,38 @@
 package Model;
 
 public class Pet {
+    String name;
+    String breed;
+    int age;
+
+
+    public Pet(int age, String breed, String name) {
+        this.age = age;
+        this.breed = breed;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
